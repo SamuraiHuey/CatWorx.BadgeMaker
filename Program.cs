fdbe99362017efbf3bbe0819e21a6b18cd15423f
Will B <tryarc@comcast.net>
2023-04-26 14:08:35 -0400

    Added starting for loop iteration and practice snippets

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>() { "adam", "amy"};
+
+            employees.Add("barbara");
+            employees.Add("billy");
+
+            for (int i =0; i < employees.Count; i++){
+                Console.WriteLine(employees[i]);
+            }
+        }
     }
-  }
 }
